431ce4ee6e5d812b0f2035a48416999b501c6917
Refandra Bramandika Miftahul Akhyar <refandika888@gmail.com>
2019-11-12 10:36:35 +0700

    index admin
    by refandra

---
+++ index_admin.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Bootstrap Sidebar Layout</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css'><link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div id="viewport">
+  <!-- Sidebar -->
+  <div id="sidebar">
+    <header>
+      <a href="#">Motor Pro</a>
+    </header>
+    <ul class="nav">
+      <li>
+        <a href="#">
+          <i class="zmdi zmdi-home"></i> Profil
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="zmdi zmdi-comment-alert"></i> Pengumuman
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="zmdi zmdi-assignment-check"></i> Absen
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="zmdi zmdi-calendar"></i> Nilai
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="zmdi zmdi-calendar-note"></i> Jadwal Pelajaran
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="zmdi zmdi-label"></i> Perizinan
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!-- Content -->
+
+  <div id="content">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="#"><i class="zmdi zmdi-notifications text-danger"></i>
+            </a>
+          </li>
+          <li><a href="#">Test User</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <h1>Simple Sidebar</h1>
+      <p>
+        Make sure to keep all page content within the llllllllllladmadjabfadas
+        <code>#content</code>.
+      </p>
+    </div>
+    <!--container fluids-->
+  </div>
+</div>
+<!-- partial -->
+
+ <!-- Footer -->
+ <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; POLIJE 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
+  
+</body>
+</html>
