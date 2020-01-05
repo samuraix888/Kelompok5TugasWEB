fc36a3b1cd102d671fa5534555707f2d00359ed8
Refandra Bramandika Miftahul Akhyar <refandika888@gmail.com>
2020-01-06 06:32:52 +0700

    salah letak
    by refandra e41180449

--- inc/kelas/hapus_kelas.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include '../koneksi.php';
-$id = $_GET['id'];
-mysqli_query($host, "DELETE FROM tb_kelas WHERE kode_kelas='$id'")or die(mysqli_error($host));
- 
-header("location:kelas.php?pesan=hapus");
-?>
