bbd467ab237b04c7198cfc96ac30c2e691827689
Refandra Bramandika Miftahul Akhyar <refandika888@gmail.com>
2020-01-06 06:40:02 +0700

    just index
    by refandra e41180449

--- index.php
+++ index.php
@@ -1,131 +1,168 @@
+<?php
+@session_start();
+include "inc/koneksi.php";
+
+if(@$_SESSION['siswa']==""){
+    header("location:inc/login/login.php");
+}
+
+?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags always come first -->
-    <title>DataPerijinan</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SMA 5 Jember</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style_user.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link rel="stylesheet" href="documentation/css/main.css">
+    <style>
+    body{
+      background-color: white;
+    }
+    .card li{
+      list-style: none;
+    }
+    </style>
+    <!--end off css-->
 
-  </head>
-  <body>
-        <!--Header-->
-<div style="background:  #00dac6;">
-<div class="banner">
+</head>
+<body>
+<!--start header-->
+<div class="banner" style="background-color: #24a319;">
   <div class="text-center">
-  <h1>SMA Negeri 5 Jember</h1>
-<h7>Sistem Monitoring Pelajar Online </h7> 
+  <h1 style="color: white;">SMA Negeri 5 Jember</h1>
+<h7 style="color: white;">Sistem Monitoring Pelajar Online </h7> 
   </div>
 
+</div>  
+<!-- end of header-->
 
+<!--start navbar-->
+
+
+
+<nav class="navbar navbar-dark py-0 bg-primary navbar-expand-lg py-md-0">
+    <button class="navbar-toggler mt-1" type="button" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fa fa-bars"></i>
+    </button>
+    <div class="navbar-collapse collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item py-0"><a href="index.php" class="nav-link">Beranda</a></li>
+            <li class="nav-item py-0"><a href="index_absen.php" class="nav-link">Absensi</a></li>
+            <li class="nav-item py-0"><a href="" class="nav-link">Nilai</a></li>
+            <li class="nav-item py-0"><a href="#" class="nav-link">Jadwal</a></li>
+        
+        </ul>
+    </div>
+</nav>
+<!--end of navbar-->
+
+<!--start body-->
+<div class="container pt-4">
+
+  <div class="row">
+    <div class="col-7">
+        <center>
+          <h4>Biodata</h4>
+           
+        </center>
+    <div class="card w-100">
   
+  <div class="card-body" style="background-color: #eceeef;">
+   
+    <?php 
+    $q = mysqli_query($host, "SELECT * FROM tb_login where username='$_SESSION[siswa]'");
+    $d = mysqli_fetch_array($q);
+		$query_mysql = mysqli_query($host, "SELECT * FROM tb_siswa where nis='$d[username]'")or die(mysqli_error($host));
+		$nomor = 1;  
+    $data = mysqli_fetch_array($query_mysql)
+      ?>
+       <div class="text-center">
+    <?php echo "<img src='assets/images/foto/$data[foto]' width='70' height='90' />";?>
+    </div>
+        <div class="card-text">
+            <li>NIS  : <?= $data['nis'] ?></li>
+            <li>NISN  : <?= $data['nisn'] ?></li>
+            <li>Nama : <?= $data['nama_siswa'] ?> </li>
+            <li>Tempat, tanggal lahir: <?= $data['tempat'].','.  $data['tanggal_lahir']?> </li>
+            <li>Alamat : <?= $data['alamat'] ?> </li>
+            
+        </div>   
+  </div>
+</div>
+    </div>
+<!--col 1-->
+
+    <div class="col-5">
+    <center>
+      <h4>
+      <div class="row responsive">
+        <div class="col-sm-8">
+           <div class="float-left" style="font-size: 20px">
+            Pengumuman 
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="float-right">
+            <a  data-toggle="collapse" href="#collapse1" style="color:#000"> <i class="fas fa-chevron-down"></i></a>
+            </div>
+        </div>
+      </div>
+        
+      </h4>
+    </center>
+
+    <div class="card w-100">
+       
+       <!-- <div id="collapse1" class="panel-collapse collapse"-->
+       <?php 
+   $query_mysql = mysqli_query($host, "SELECT * FROM tb_pengumuman")or die(mysqli_error($host));
+   $nomor = 1;
+   while($data = mysqli_fetch_array($query_mysql)){
+   ?>
+   
+  
+     <td><a  data-toggle="collapse" href="#collapse1" style="color:#000"><?php echo $data['judul']; ?></a></td>
+     <div id="collapse1" class="panel-collapse collapse">
+     <?php echo $data['pengumuman']; ?> </div>
+
+    
+     <?php } ?>
+  
+      
+ </div>
+ </div>
+   
+</div>
+<!--col 2-->
+<!--end of row-->
+<br>
+</div>
+<!--end of body-->
+
+<div class="footer"style="background-color :transparent;">
+<center>
+  <p>Copyright &copy;POLIJE 2019 - MotorPro</p>
+</center>
+</div>
+<!--end of navbar-->
 
 </div>
-<div style="background:  #009688;">
-<nav class="navbar navbar-expand-lg navbar-light border border-dark mt-2">
-    
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index_user.php">Beranda <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Absen</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Jadwal</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Nilai</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Lain-Lain
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="index.php">Perizinan</a>
-        </div>
-      </li>
-      
-    </ul>
-
-    <ul class="navbar-nav float-right">
-    <li class="nav-item">
-    <a class="nav-link" href="#" style="color: black;">Username</a>
-    </li>
-
-    <li class="nav-item">
-    <a class="nav-link" href="#" style="color: black;">Keluar</a>
-    </li>
-    </ul>
-    
-        
-        
-  </div>
-  </div>
-</nav>
-</div>        
-
-                  <!--Akhir Header-->
-                  
-
-
-    <div id="wrapper">
-
-      <nav class="navbar navbar-light bg-faded">
-      <a>DataPerijinan</a>
-      <ul class="nav navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php"><span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="index.php?page=add"></a>
-        </li>
-      </ul>
-      </nav>
-
-      <div class="container box">
-       <?php include "add.php";?>
-      </div>
-
-    </div>
-
-
-    <!--Foter-->
-    <footer>
-       <div class="container text-center">
-        <div class="row">
-        <p>&copf; Copyright 2019 |built by :MotorPro <i class="glyphicon glyphicon-heart"></i>by.<a href="https://www.facebook.com/ansory.ansory.399" class="btn btn-danger"><i class="glyphicon glyphicon-play"></i>Contac Me</a></p>
-
-        </div>
-
-       </div>
-      </footer>
-
-      <!--akhirFoter-->
-
-    <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="ckeditor/ckeditor.js"></script>
-    <script type="text/javascript">
-     <script src="js/jquery-3.2.1.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/script.js"></script>
+<!--end of container-->
+</div> 
+</div> 
+</html>
+</body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </script>
-  </body>
+<!--end of js-->
+
 </html>
+<?php
+
+?>
